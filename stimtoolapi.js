757212ad601aee0382a0c352c3d29d5fc1dbaae8
James Touthang <jtouthang@laureateinstitute.org>
2022-02-09 11:59:43 -0600

    remove mail gun

--- stimtoolapi.js
+++ stimtoolapi.js
@@ -13,14 +13,14 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 // const client = require('twilio')(accountSid, authToken);
 
 const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
+// const Mailgun = require('mailgun.js');
+// const mailgun = new Mailgun(formData);
 
-const mg = mailgun.client({
-    username: 'api',
-    key: process.env.MAILGUN_API_KEY,
-    public_key: process.env.MAILGUN_PUBLIC_KEY
-});
+// const mg = mailgun.client({
+//     username: 'api',
+//     key: process.env.MAILGUN_API_KEY,
+//     public_key: process.env.MAILGUN_PUBLIC_KEY
+// });
 
 // var mailgun = require('mailgun-js')({ apiKey: mailgun_api_key, domain: mailgun_domain });
 
