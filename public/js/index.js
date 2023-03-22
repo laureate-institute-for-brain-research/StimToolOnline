c6349b8e507ae548b89e2ffc739a8228c466f95c
James Touthang <jtouthang@laureateinstitute.org>
2023-03-22 00:12:54 -0500

    * studies: added the 3 sessions for invitiatin task. and studie session is now dynamically loaded.

--- public/js/index.js
+++ public/js/index.js
@@ -150,6 +150,23 @@ $('#study-list').change(function () {
     document.getElementById('id_div').style.display = 'block';
     document.getElementById('session_div').style.display = 'block'; // Show session dropdown
 
+    studies_sessions = {
+        'AAC-BET' : 2,
+        'BK_Pilot': 2,
+        'METH_Pilot': 2,
+        'CognitiveControl': 2,
+        'NCAIR': 1,
+        'NCAIR-Arousal': 1,
+        'Driving2': 2,
+        'WB_Pilot': 1,
+        'WBMTURK_Social_Media': 1,
+        'WBMTURK_Blind_Dating': 2,
+        'WBMTURK_Emotional_Faces': 2,
+        'WBMTURK_Cooperation_Task': 2,
+        'WBMTURK_Invitation_Task': 3
+    }
+
+    
     two_session_studies = [
         'AAC-BET',
         'BK_Pilot',
@@ -161,13 +178,49 @@ $('#study-list').change(function () {
         'WBMTURK_Cooperation_Task',
         'WBMTURK_Invitation_Task'
     ]
-    if (!two_session_studies.includes(document.getElementById('study-list').value)) {
-        var selectobject = document.getElementById("session-list");
-        for (var i=0; i<selectobject.length; i++) {
-            if (selectobject.options[i].value == 'T2')
-                selectobject.remove(i);
+
+    // verify that the list of session list is the number of sessions
+    let study_ = document.getElementById('study-list').value;
+    var sessions_ = document.getElementById("session-list");
+    let max_session = studies_sessions[study_];
+    console.log(study_, max_session);
+    // sessions_.options[sessions_.options.length] = new Option('T2', 'T2')
+    //Create and append select list
+    // clreat all child
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
         }
     }
+    removeAllChildNodes(sessions_);
+
+    // add the disabled option
+    var option_disabled = document.createElement("option");
+    option_disabled.selected = true;
+    option_disabled.disabled = true;
+    option_disabled.text = 'Choose Session';
+    
+    sessions_.appendChild(option_disabled);
+
+    // <select id='session-list' required class="form-control" name='session'>
+    //                           <option selected disabled>Choose Session</option>
+    //                           <option value="T1">T1</option>
+    //                       </select>
+
+    //Create and append the options
+    for (var i = 1; i <= max_session; i++) {
+        var option = document.createElement("option");
+        option.value = 'T'+i;
+        option.text = 'T'+i;
+        sessions_.appendChild(option);
+    }
+    // if (!two_session_studies.includes(document.getElementById('study-list').value)) {
+    //     var selectobject = document.getElementById("session-list");
+    //     for (var i=0; i<selectobject.length; i++) {
+    //         if (selectobject.options[i].value == 'T2')
+    //             selectobject.remove(i);
+    //     }
+    // }
 
     mobileonly_studies = ['Driving2']
     // Fire modal to aler user that this study is mobile
