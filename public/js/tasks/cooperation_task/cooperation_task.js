6e7672e63ff370f0bf5adbe8659724eb762a61c8
Aardron Robinson <arobinson@laureateinstitute.org>
2023-05-25 17:46:02 +0000

    Cooperation: Added new schedules. Implemented new saving strategy.

--- public/js/tasks/cooperation_task/cooperation_task.js
+++ public/js/tasks/cooperation_task/cooperation_task.js
@@ -38,6 +38,9 @@ var score_two_neg = 0;
 var score_three_neg = 0;
 
 var trials_data = []
+var main_loop_count = 0
+var last_trial_num = 0
+var total_requests = 0
 var g = {}				// global variables
 g.PLEASANT_COLOR = '#00ff00' //'green';
 g.UNPLEASANT_COLOR = '#ff0000' //'red';
@@ -722,7 +725,7 @@ function experimentInit() {
 	thanksText = new visual.TextStim({
 		win: psychoJS.window,
 		name: 'thanksText',
-		text: 'This is the end of the task run.\n\nThanks!',
+		text: 'This is the end of the task run. Please wait for the upcoming survey and for all task data to be saved. Thank you!',
 		font: 'Arial',
 		units: 'height',
 		pos: [0, 0], height: 0.05, wrapWidth: undefined, ori: 0,
@@ -1460,6 +1463,9 @@ function trialsLoopBegin(thisScheduler) {
 		seed: undefined, name: 'trials'
 	});
 
+	main_loop_count = 0
+	last_trial_num = blocks.nTotal
+
 	g.global_trial_number = 0;
 	g.game_number = 1;
 	//randomizePair(blocks) // randomize outcome
@@ -1499,6 +1505,9 @@ function trialsLoopBegin2(thisScheduler) {
 		trialList: 'run_schedule2.xls',
 		seed: undefined, name: 'trials'
 	});
+
+	main_loop_count = 0
+	last_trial_num = blocks.nTotal
 
 	g.global_trial_number = 0;
 	g.game_number = 1;
@@ -2413,7 +2422,7 @@ function blockRoutineTrials(trials) {
 			mark_event(trials_data, globalClock, g.trial_number, trial_type, event_types['TRIAL_ONSET'],
 				'NA', 'NA', 'NA')
 			
-			sendData()
+			//sendData()
 			g.new_trial_marked = true;
 		}
 
@@ -2766,6 +2775,7 @@ function initialFixation(trials) {
 function sendData() {
 	//console.log(trials_data)
 	//console.log(expInfo)
+	total_requests += 1
 	$.ajax({
         type: "POST",
         url: '/savecoop',
@@ -2773,11 +2783,21 @@ function sendData() {
 			"trials_data": trials_data,
 			"expInfo": expInfo
 		},
-		dataType: 'JSON',
+		// dataType: 'JSON',
 		success:function(data) {
 			console.log(data)
 		  }
-    })
+	})
+	.done(function (data) {
+		total_requests -= 1
+		console.log("success:")
+		console.log(Date.now())
+		console.log(data)
+	})
+		.fail(function (err) {
+		console.log("ERR:")
+		console.log(err)	
+	})
 }
 
 /**
@@ -2960,6 +2980,10 @@ function thanksRoutineEachFrame(trials) {
 				break;
 			}
 
+		if (total_requests > 0)
+		{
+			continueRoutine = true
+		}
 		// refresh the screen if continuing
 		if (continueRoutine && routineTimer.getTime() > 0) {
 			return Scheduler.Event.FLIP_REPEAT;
@@ -3003,7 +3027,18 @@ function endLoopIteration(thisScheduler, loop = undefined) {
 				}
 			}
 		}
-		sendData()
+
+		main_loop_count += 1
+		if (main_loop_count % 1 == 0) {
+			console.log("sending data")
+			sendData()
+		}
+		else if (main_loop_count == last_trial_num)
+		{
+			console.log("sending data")
+			sendData()
+		}
+		//sendData()
 
 		return Scheduler.Event.NEXT;
 	};
